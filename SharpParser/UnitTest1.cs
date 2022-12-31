94c655cc1a985cb85deb962cd1452d4b88a86e38
Darío Cutillas Carrillo <git@dario.nu>
2022-12-31 15:58:35 +0100

    Initial commit

---
+++ SharpParser/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace SharpParser;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
